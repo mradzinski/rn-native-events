534459371c68f7401dda597017bc724509a0c89e
Matias <mradzinski@gmail.com>
2018-06-25 15:19:09 -0300

    first commit

---
+++ library/src/main/java/com/lufinkey/react/eventemitter/RNEventConformer.java
@@ -0,0 +1,11 @@
+package com.lufinkey.react.eventemitter;
+
+public interface RNEventConformer {
+    void __registerAsJSEventEmitter(int moduleId);
+
+    void onNativeEvent(String eventName, Object... args);
+
+    void onJSEvent(String eventName, Object... args);
+
+    void onEvent(String eventName, Object... args);
+}
